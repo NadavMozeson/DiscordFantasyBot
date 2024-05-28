7b792541494cf22b84d624be48c42f92264a190e
NadavMozeson <nadavson02@gmail.com>
2024-05-28 13:22:24 +0300

    Startup

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
---
+++ private_data.py
@@ -0,0 +1,7 @@
+# This file is a private data file for a MongoDB connection.
+
+USERNAME = '???'
+PASSWORD = '???'
+IP = '???'
+PORT = '???'
+CONNECTION_URL = f"mongodb://{USERNAME}:{PASSWORD}@{IP}:{PORT}"
